fc86cf79aa3dd2b47f964e85324277712bd55dde
Nicolas Cervino <nicolascervino2@gmail.com>
2022-08-18 16:41:55 -0300

    se movio la clase user y movie a sus propios archivos js

--- js/app.js
+++ js/app.js
@@ -9,33 +9,6 @@ function filtrarPorGenero(genero) {
     return resultado;
 }
 
-// Clase usuario, por ahora solo posee un nombre y una lista de peliculas
-class User {
-    constructor(username) {
-        this.username = username;
-        this.myList = [];
-        // Guardo la lista al inicializar el usuario en el localStorage
-        localStorage.setItem("listaPeliculas", JSON.stringify(this.myList));
-    }
-    // Metodo que permite agregar una pelicula a la lista del usuario
-    agregarAMiLista(pelicula) {
-        if (!this.myList.includes(pelicula)) {
-            this.myList.push(pelicula);
-            // Agrega la pelicula al local storage
-            localStorage.setItem("listaPeliculas", JSON.stringify(this.myList));
-        }
-    }
-    // Metodo que permite eliminar una pelicula de la lista del usuario
-    borrarDeMiLista(pelicula) {
-        let indice = this.myList.indexOf(pelicula);
-        if (indice != -1) {
-            this.myList.splice(indice, 1);
-            // Actualizo la lista en el local storage
-            localStorage.setItem("listaPeliculas", JSON.stringify(this.myList));
-        }
-    }
-}
-
 // Permite crear una card para una pelicula
 function crearCard(pelicula) {
     let codigoCard = `  <div class="col-6 col-sm-2 p-2">
---
+++ js/movie.class.js
@@ -0,0 +1,11 @@
+class Movie {
+    constructor(tittle, cast, genre, duration, img, img2, description) {
+        this.tittle = tittle;
+        this.cast = cast;
+        this.genre = genre;
+        this.duration = duration;
+        this.imgDesktop = img;
+        this.imgMobile = img2;
+        this.description = description;
+    }
+}
